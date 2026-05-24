db0fbd9a8f2195549ab9957b0f6bfde558606c39
Ami Hiramatsu <ami_hira03@yahoo.co.jp>
2026-05-25 05:01:52 +0900

    ✨ feat(api): 商品一覧取得APIを追加

---
+++ src/app/Http/Controllers/Api/ItemController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Item;
+use Illuminate\Http\Request;
+
+class ItemController extends Controller
+{
+    public function index()
+    {
+        $items = Item::with('purchase')
+            ->latest()
+            ->get();
+
+        return response()->json($items);
+    }
+}
