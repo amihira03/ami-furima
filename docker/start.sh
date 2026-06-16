f3e59934fa6ac9dd3951bd217c4f667c8eef6338
Ami Hiramatsu <ami_hira03@yahoo.co.jp>
2026-06-16 11:02:10 +0900

    🐛 fix(docker): NginxのポートをRAILWAY環境変数PORTに対応

--- docker/start.sh
+++ docker/start.sh
@@ -3,10 +3,16 @@ cd /var/www
 php artisan config:cache
 php artisan storage:link
 
+# PORTが未設定の場合は8080をデフォルトに
+export PORT=${PORT:-8080}
+
+# Nginx設定にPORTを反映
+envsubst '${PORT}' < /etc/nginx/conf.d/default.conf > /tmp/default.conf
+cp /tmp/default.conf /etc/nginx/conf.d/default.conf
+
 # PHP-FPM をバックグラウンドで起動
 php-fpm -D
 
-# PHP-FPMの起動を待つ
 sleep 2
 
 # Nginx をフォアグラウンドで起動
