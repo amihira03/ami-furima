a726ecae2f29130e3fdeec8983fc0a4f6c88d307
Ami Hiramatsu <ami_hira03@yahoo.co.jp>
2026-06-16 11:06:48 +0900

    🐛 fix(docker): envsubstをsedに置き換え

--- docker/start.sh
+++ docker/start.sh
@@ -4,11 +4,10 @@ php artisan config:cache
 php artisan storage:link
 
 # PORTが未設定の場合は8080をデフォルトに
-export PORT=${PORT:-8080}
+PORT=${PORT:-8080}
 
 # Nginx設定にPORTを反映
-envsubst '${PORT}' < /etc/nginx/conf.d/default.conf > /tmp/default.conf
-cp /tmp/default.conf /etc/nginx/conf.d/default.conf
+sed -i "s/\${PORT}/$PORT/g" /etc/nginx/conf.d/default.conf
 
 # PHP-FPM をバックグラウンドで起動
 php-fpm -D
