c5e2425d9e8bc6c2ab65eccfa4cf9c0c653415ce
Ami Hiramatsu <ami_hira03@yahoo.co.jp>
2026-06-16 11:14:04 +0900

    🔧 chore(env): APIのbaseURLをRailwayのURLに変更

--- src/frontend/src/lib/axios.ts
+++ src/frontend/src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost/api",
+    baseURL: "https://ami-furima-production.up.railway.app/api",
     headers: {
         "Content-Type": "application/json",
         Accept: "application/json",
